58ea04f07610c672867f800fb786657282d0ac22
hennr <hennr@hennr.name>
2014-09-27 14:43:16 +0200

    load fighter model

--- src/main/java/septemberjam/GameApplication.java
+++ src/main/java/septemberjam/GameApplication.java
@@ -4,22 +4,16 @@ import com.jme3.app.SimpleApplication;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Mesh;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
 
-/**
- * @author andreas
- */
 public class GameApplication extends SimpleApplication {
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
 
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);
+        Spatial fighterSpatial = assetManager.loadModel("Models/fighter.j3o");
+        rootNode.attachChild(fighterSpatial);
     }
 
     public static void main(String[] args) {
