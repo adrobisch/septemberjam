40c615aef7c5392c660bd917125ecadebb43c10c
hennr <hennr@hennr.name>
2014-09-27 15:08:04 +0200

    set fighter position and rotation

--- src/main/java/septemberjam/GameApplication.java
+++ src/main/java/septemberjam/GameApplication.java
@@ -1,19 +1,22 @@
 package septemberjam;
 
 import com.jme3.app.SimpleApplication;
-import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
-import com.jme3.scene.Geometry;
-import com.jme3.scene.Mesh;
+import com.jme3.math.FastMath;
 import com.jme3.scene.Spatial;
-import com.jme3.scene.shape.Box;
 
 public class GameApplication extends SimpleApplication {
     @Override
     public void simpleInitApp() {
 
-        Spatial fighterSpatial = assetManager.loadModel("Models/fighter.j3o");
-        rootNode.attachChild(fighterSpatial);
+        Spatial fighter = assetManager.loadModel("Models/fighter.j3o");
+        fighter.scale(0.7f);
+        fighter.rotate(0, degreeAsRadian(180), 0);
+        fighter.setLocalTranslation(0, -2.5f, 0);
+        rootNode.attachChild(fighter);
+    }
+
+    public float degreeAsRadian(float degree) {
+        return (degree / 180 * FastMath.PI);
     }
 
     public static void main(String[] args) {
